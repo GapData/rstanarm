980706ebb335c19cdb38c93c2c9d475f809746b9
jgabry <jgabry@gmail.com>
2017-03-15 23:44:49 -0400

    Pass ... to psislw, transform to posterior_linpred

--- R/loo-prediction.R
+++ R/loo-prediction.R
@@ -8,10 +8,8 @@
 #' @param lw An optional matrix of (smoothed) log-weights. If \code{lw} is 
 #'   missing then \code{\link[loo]{psislw}} is executed internally, which may be
 #'   time consuming for large amounts of data.
-#' @param ... For \code{loo_predict}, arguments passed to 
-#'   \code{\link{posterior_predict}} (e.g. \code{newdata}). For
-#'   \code{loo_linpred}, arguments passed to \code{\link{posterior_linpred}} 
-#'   (e.g., \code{transform}, \code{newdata}).
+#' @param ... Optional arguments passed to \code{\link[loo]{psislw}} if 
+#'   \code{lw} is not specified.
 #'   
 #' @return A vector with \code{nrow(newdata)} elements.
 #' 
@@ -29,30 +27,32 @@
 #' pred <- loo_predict(fit)
 #' 
 loo_predict.stanreg <- function(object, lw, ...) {
-  wts <- make_weights(object, lw)
-  preds <- posterior_predict.stanreg(object, ...)
+  wts <- make_weights(object, lw, ...)
+  preds <- posterior_predict(object)
   colSums(preds * wts)
 }
 
 #' @rdname loo_predict.stanreg
 #' @export
-#'   
-loo_linpred.stanreg <- function(object, lw, ...) {
-  wts <- make_weights(object, lw)
-  linpreds <- posterior_linpred.stanreg(object, ...)
+#' @param transform Passed to \code{\link{posterior_linpred}}.
+#'    
+loo_linpred.stanreg <- function(object, lw, transform = FALSE, ...) {
+  wts <- make_weights(object, lw, ...)
+  linpreds <- posterior_linpred(object, transform = transform)
   colSums(linpreds * weight_matrix)
 }
 
 
 # internal ----------------------------------------------------------------
 
-# @param object,lw Same as above.
-make_weights <- function(object, lw) {
+# @param object,lw,... Same as above.
+# @return A matrix.
+make_weights <- function(object, lw, ...) {
   if (!missing(lw)) {
     stopifnot(is.matrix(lw))
     return(exp(lw))
   }
-  psis <- loo::psislw(llfun = ll_fun(object), llargs = ll_args(object))
+  psis <- loo::psislw(llfun = ll_fun(object), llargs = ll_args(object), ...)
   exp(psis[["lw_smooth"]]) 
 }
 
