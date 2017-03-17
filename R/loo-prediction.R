1a5bd5e67c07c7951fcaf4fc26ad687a0f821b73
jgabry <jgabry@gmail.com>
2017-03-17 17:31:46 -0400

    very rough draft of loo_predictive_interval

--- R/loo-prediction.R
+++ R/loo-prediction.R
@@ -1,7 +1,7 @@
 #' Compute weighted expectations using LOO
 #' 
 #' @export
-#' @aliases loo_predict loo_linpred loo_pit
+#' @aliases loo_predict loo_linpred loo_pit loo_predictive_interval
 #' 
 #' @templateVar stanregArg object
 #' @template args-stanreg-object
@@ -13,25 +13,27 @@
 #' @param ... Optional arguments passed to \code{\link[loo]{psislw}} if 
 #'   \code{lw} is not specified.
 #'   
-#' @return A vector with one element per observation.
+#' @return \code{loo_predictive_interval} returns a matrix with one row per
+#'   observation and two columns. The other functions return a vector with one
+#'   element per observation.
 #' 
-#' @examples 
-#' ### Logistic regression
-#' head(wells)
-#' wells$dist100 <- wells$dist / 100
-#' fit <- stan_glm(
-#'   switch ~ dist100 + arsenic, 
-#'   data = wells, 
-#'   family = binomial(link = "logit"), 
-#'   QR = TRUE,
-#'   chains = 2, iter = 200 # for speed of example only
-#' )
-#' pred <- loo_predict(fit)
+#' @examples
+#' fit <- stan_glm(mpg ~ wt, data = mtcars)
+#' loo_preds <- loo_predict(fit)
+#' preds <- colMeans(posterior_predict(fit))
+#' 
+#' library("ggplot2")
+#' ggplot() + 
+#'  geom_line(aes(x = seq_along(preds), y = preds), color = "black", size = 2) + 
+#'  geom_line(aes(x = seq_along(loo_preds), y = loo_preds), color = "gray", size = 1) + 
+#'  labs(x = "Data point", y = "Mean") +
+#'  bayesplot::theme_default()
 #' 
 loo_predict.stanreg <-
   function(object, 
            lw, 
-           type = c("mean", "var"), 
+           type = c("mean", "var", "quantile"), 
+           probs = 0.5,
            ...) {
     
     type <- match.arg(type)
@@ -40,24 +42,42 @@ loo_predict.stanreg <-
     if (is_polr(object) && !is_scobit(object))
       preds <- polr_yrep_to_numeric(preds)
     
-    loo_expectation(preds, wts, type)
+    loo_expectation(preds, wts, type, probs)
   }
 
 #' @rdname loo_predict.stanreg
 #' @export
+#' @param prob For \code{loo_predictive_interval}, a scalar in \eqn{(0,1)}
+#'   indicating the desired probability mass to include in the intervals. The
+#'   default is \code{prob=0.9} (\eqn{90}\% intervals).
+loo_predictive_interval.stanreg <- function(object, lw, prob = 0.9, ...) {
+  stopifnot(length(prob) == 1)
+  alpha <- (1 - prob) / 2
+  probs <- c(alpha, 1 - alpha)
+  labs <- paste0(100 * probs, "%")
+  intervals <- t(loo_predict(object, lw, type = "quantile", probs))
+  colnames(intervals) <- labs
+  return(intervals)
+}
+
+#' @rdname loo_predict.stanreg
+#' @export
 #' @param transform Passed to \code{\link{posterior_linpred}}.
+#' @param probs If \code{type} is \code{"quantile"}, a vector of probabilities
+#'   in \eqn{(0,1)}. Ignored if \code{type} is \code{"mean"} or \code{"var"}.
 #'    
 loo_linpred.stanreg <-
   function(object,
            lw,
-           type = c("mean", "var"),
+           type = c("mean", "var", "quantile"),
+           probs = 0.5,
            transform = FALSE,
            ...) {
     
     type <- match.arg(type)
     wts <- loo_weights(object, lw, ...)
     linpreds <- posterior_linpred(object, transform = transform)
-    loo_expectation(linpreds, wts, type)
+    loo_expectation(linpreds, wts, type, probs)
   }
 
 #' @rdname loo_predict.stanreg
@@ -97,21 +117,47 @@ loo_weights <- function(object, lw, log = FALSE, ...) {
 
 # @param X,W matrices of the same size
 # @param type either "mean" or "var"
-loo_expectation <- function(X, W, type) {
+loo_expectation <- function(X, W, type, probs) {
   fun <- switch(
     type,
     "mean" = function(x, w)
       sum(w * x),
     "var" = function(x, w)
-      sum(w * (x - sum(w * x)) ^ 2)
+      sum(w * (x - sum(w * x)) ^ 2), 
+    "quantile" = wprctile
   )
+  if (type == "quantile") {
+    formals(fun)[["probs"]] <- probs
+    funval <- numeric(length(probs))
+  } else {
+    funval <- numeric(1)
+  }
   vapply(seq_len(ncol(X)), function(j) {
     fun(X[, j], W[, j])
-  }, FUN.VALUE = numeric(1))
+  }, FUN.VALUE = funval)
 }
 
-# @param x A numeric vector
-exp_log_sum_exp <- function(x) {
-  max_x <- max(x, na.rm = TRUE)
-  exp(max_x + log(sum(exp(x - max_x))))
+
+# @param x,w vectors of the same length
+# @param p vector of probabilities
+wprctile <- function(x, w, probs = 0.5) {
+  stopifnot(length(x) == length(w), all(probs > 0 | probs < 1))
+  
+  x <- sort(x)  
+  ww <- cumsum(w)
+  ww <- ww / ww[length(ww)]
+
+  y <- numeric(length(probs))
+  for (j in seq_along(probs)) {
+    ids <- which(ww >= probs[j])
+    wi <- min(ids)
+    if (wi == 1) {
+      y[j] <- x[1]
+    } else {
+      w1 <- ww[wi - 1]
+      x1 <- x[wi - 1]
+      y[j] <- x1 + (x[wi] - x1) * (probs[j] - w1) / (ww[wi] - w1)
+    }
+  }
+  return(y)
 }
