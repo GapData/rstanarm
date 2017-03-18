bf152968d66c1744c90c0c90859b993332bf372a
jgabry <jgabry@gmail.com>
2017-03-18 13:37:19 -0400

    Update loo-prediction
    [ci skip]

--- R/loo-prediction.R
+++ R/loo-prediction.R
@@ -18,16 +18,16 @@
 #'   element per observation.
 #' 
 #' @examples
-#' fit <- stan_glm(mpg ~ wt, data = mtcars)
-#' loo_preds <- loo_predict(fit)
-#' preds <- colMeans(posterior_predict(fit))
+#' # data from help("lm")
+#' ctl <- c(4.17,5.58,5.18,6.11,4.50,4.61,5.17,4.53,5.33,5.14)
+#' trt <- c(4.81,4.17,4.41,3.59,5.87,3.83,6.03,4.89,4.32,4.69)
+#' d <- data.frame(
+#'   weight = c(ctl, trt), 
+#'   group = gl(2, 10, 20, labels = c("Ctl","Trt"))
+#' ) 
+#' fit <- stan_glm(weight ~ group, data = d)
+#' loo_predictive_interval(fit, prob = 0.8)
 #' 
-#' library("ggplot2")
-#' ggplot() + 
-#'  geom_line(aes(x = seq_along(preds), y = preds), color = "black", size = 2) + 
-#'  geom_line(aes(x = seq_along(loo_preds), y = loo_preds), color = "gray", size = 1) + 
-#'  labs(x = "Data point", y = "Mean") +
-#'  bayesplot::theme_default()
 #' 
 loo_predict.stanreg <-
   function(object, 
@@ -55,9 +55,9 @@ loo_predictive_interval.stanreg <- function(object, lw, prob = 0.9, ...) {
   alpha <- (1 - prob) / 2
   probs <- c(alpha, 1 - alpha)
   labs <- paste0(100 * probs, "%")
-  intervals <- t(loo_predict(object, lw, type = "quantile", probs))
-  colnames(intervals) <- labs
-  return(intervals)
+  intervals <- loo_predict(object, lw, type = "quantile", probs)
+  rownames(intervals) <- labs
+  t(intervals)
 }
 
 #' @rdname loo_predict.stanreg
@@ -99,34 +99,35 @@ loo_pit.stanreg <- function(object, lw, ...) {
 # internal ----------------------------------------------------------------
 
 # @param object,lw,... Same as above.
+# @param log If FALSE (default) the weights are exponentiated before returning
 # @return A matrix.
 loo_weights <- function(object, lw, log = FALSE, ...) {
   if (!missing(lw)) {
     stopifnot(is.matrix(lw))
   } else {
-    message("'lw' argument was not specified. Running PSIS to compute weights...")
+    message("'lw' argument not specified. Running PSIS to compute weights...")
     psis <- loo::psislw(llfun = ll_fun(object), llargs = ll_args(object), ...)
     lw <- psis[["lw_smooth"]]
   }
   if (log) 
     return(lw) 
-  else 
-    return(exp(lw))
+  
+  exp(lw)
 }
 
-
+# Compute weighted expectations
+#
 # @param X,W matrices of the same size
-# @param type either "mean" or "var"
+# @param type either "mean", "var", or "quantile"
+# @param probs a vector of probabilities if type="quantile"
 loo_expectation <- function(X, W, type, probs) {
-  fun <- switch(
-    type,
-    "mean" = function(x, w)
-      sum(w * x),
-    "var" = function(x, w)
-      sum(w * (x - sum(w * x)) ^ 2), 
-    "quantile" = wprctile
-  )
+  stopifnot(identical(dim(X), dim(W)))
+  fun <- switch(type,
+                "mean" = .wmean,
+                "var" = .wvar,
+                "quantile" = .wquant)
   if (type == "quantile") {
+    stopifnot(all(probs > 0 | probs < 1))
     formals(fun)[["probs"]] <- probs
     funval <- numeric(length(probs))
   } else {
@@ -138,11 +139,20 @@ loo_expectation <- function(X, W, type, probs) {
 }
 
 
-# @param x,w vectors of the same length
-# @param p vector of probabilities
-wprctile <- function(x, w, probs = 0.5) {
-  stopifnot(length(x) == length(w), all(probs > 0 | probs < 1))
-  
+# loo-weighted mean, variance, and quantiles
+#
+# @param x,w vectors of the same length. this should be checked inside
+#   loo_expectation() before calling these functions.
+# @param probs vector of probabilities.
+#
+.wmean <- function(x, w) {
+  sum(w * x)
+}
+.wvar <- function(x, w) {
+  r <- (x - .wmean(x, w))^2
+  sum(w * r)
+}
+.wquant <- function(x, w, probs) {
   x <- sort(x)  
   ww <- cumsum(w)
   ww <- ww / ww[length(ww)]
